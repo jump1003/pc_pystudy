46c303f67e90e9013d56f594b485427a9d7bee4e
jump1003 <171149320@qq.com>
2017-10-12 10:19:15 +0800

    提交Find_char

--- IOlearn/Find_char.py
+++ IOlearn/Find_char.py
@@ -1,3 +1,16 @@
 import os
-def Find_char(filechar):
-	pass
+def Find_char(filechar, Path = os.path.abspath('.')):
+	#os.chdir(Path)
+	# new_path = []
+	#print('get in %s' %Path)
+	for f in os.listdir(Path):
+		os.chdir(Path)
+		if os.path.isdir(f):
+			Find_char(filechar,os.path.join(Path, f))
+		# elif f.find(filechar) != -1:
+		elif filechar in f:
+			print(os.path.join(Path, f))
+
+
+if __name__== '__main__':
+	Find_char('.')
