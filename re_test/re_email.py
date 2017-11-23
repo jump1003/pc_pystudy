fe0dd9ffa08b43425d0733b693be28f641889b4d
jump1003 <171149320@qq.com>
2017-11-23 16:54:23 +0800

    提取邮箱中名字

--- re_test/re_email.py
+++ re_test/re_email.py
@@ -7,22 +7,27 @@ import re
 #			bill.gates@microsoft.com
 
 #re_email = re.compile(r'^[a-zA-Z0-9.]+@[a-zA-Z0-9.]+\.com$')
-re_email = re.compile(r'^[a-zA-Z0-9.]+@(\[?)[a-zA-Z0-9\-.]+\.([a-zA-Z]{2,3}|[0-9]{1,3})(\]?)$')
-def is_email(addr):
-	if re_email.match(addr):
-		print('[%s]...is a email.' % addr)
-	else:
-		print('[%s]...is not a email.' % addr)
+# re_email = re.compile(r'^[a-zA-Z0-9.]+@(\[?)[a-zA-Z0-9\-.]+\.([a-zA-Z]{2,3}|[0-9]{1,3})(\]?)$')
+# def is_email(address):
+# 	if re_email.match(address):
+# 		print('[%s]...is a email.' % address)
+# 	else:
+# 		print('[%s]...is not a email.' % address)
 
 
 # 版本二可以提取出带名字的Email地址:
 #    <Tom Paris> tom@voyager.org => Tom Paris
 #    bob@example.com => bob
 
-#re_email_n = re.compile(r'')
+re_email_n = re.compile(r'<?([a-zA-Z0-9\s]*)>?\s?([a-zA-Z0-9]*)@([a-zA-Z0-9]+)\.([a-zA-Z]{2,3}|[0-9]{1,3})$')
+def name_email(address):
+	print(re_email_n.match(address).group(1))
+
 
 if __name__ == '__main__':
-	is_email('someone@gmail.net')
-	is_email('bill.gates@microsoft.cn')
-	is_email('bob#example.com')
-	is_email('mr-bob@example.com')
+	# is_email('someone@gmail.net')
+	# is_email('bill.gates@microsoft.cn')
+	# is_email('bob#example.com')
+	# is_email('mr-bob@example.com')
+	name_email('<Tom Paris> tom@voyager.org')
+	name_email('bob@example.com')
