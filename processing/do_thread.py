0e19f11a36ec1bb01d3cf5f40f6553ea868ae058
jump1003 <171149320@qq.com>
2018-01-19 17:32:30 +0800

    anthing

--- processing/do_thread.py
+++ processing/do_thread.py
@@ -1,20 +1,44 @@
-import time, threading
+# import time, threading
+#
+#
+# def loop_t():
+# 	print('Thread %s is running...' % threading.current_thread ().name)
+# 	n = 0
+# 	while 1:
+# 		n += 1
+# 		start = time.time()
+# 		time.sleep(1)
+# 		end = time.time()
+# 		print('Thread %s >>> %s,using %.2f seconds' %(threading.current_thread().name, n, (end-start)))
+# 	print('Thread %s ended.' % threading.current_thread ().name)
+#
+#
+# print('Thread %s is running...' % threading.current_thread().name)
+# t = threading.Thread(target=loop_t, name='LoopThread')
+# t.start()
+# # t.join()
+# print('Thread %s ended.' % threading.current_thread().name)
+# -*- coding: utf-8 -*-
+import threading
+import time
 
 
-def loop_t():
-	print('Thread %s is running...' % threading.current_thread ().name)
-	n = 0
-	while n < 5:
-		n += 1
-		start = time.time()
-		time.sleep(1)
-		end = time.time()
-		print('Thread %s >>> %s,using %.2f seconds' %(threading.current_thread().name, n, (end-start)))
-	print('Thread %s ended.' % threading.current_thread ().name)
+class Test (object):
+	def __init__(self):
+		# threading.Thread.__init__(self)
+		self._sName = "machao"
+		self.process()
+		
+	def process(self):
+		# args是关键字参数，需要加上名字，写成args=(self,)
+		th1 = threading.Thread (target=Test.buildList, args=(self,))
+		th1.start()
+		th1.join()
+	
+	def buildList(self):
+		while True:
+			print("start")
+			time.sleep (3)
 
 
-print('Thread %s is running...' % threading.current_thread().name)
-t = threading.Thread(target=loop_t, name='LoopThread')
-t.start()
-t.join()
-print('Thread %s ended.' % threading.current_thread().name)
+test = Test()
--- re_test/CODE_YANZHENG.py
+++ re_test/CODE_YANZHENG.py
@@ -1,35 +1,47 @@
 from PIL import Image, ImageDraw, ImageFont, ImageFilter
 import random
 
-# 随机字母
-def rndchr():
-	return chr(random.randint(65, 90))
+class CheckCode(object):
+	# 随机字母
+	def rndchr(self):
+		return chr(random.randint(65, 90))
+	
+	# 背景颜色
+	def rndcolor_b(self):
+		return random.randint(127, 255), random.randint(127, 255), random.randint(127, 255)
+	
+	# 字体颜色
+	def rndcolor_n(self):
+		return random.randint(50, 127), random.randint(50, 127), random.randint(50, 127)
+	
+	def get_code(self):
+		# 100*25
+		width = 20 * 4
+		height = 20
+		code_list = ''
+		global image
+		image = Image.new('RGB', (width, height), (255, 255, 255))
+		# 创建Font
+		font = ImageFont.truetype('arial.ttf', 20)
+		# 创建Draw
+		draw = ImageDraw.Draw(image)
+		# 填充像素
+		for x in range(width):
+			for y in range(height):
+				draw.point((x, y), fill=self.rndcolor_b())
+		# 输出文字
+		for t in range(4):
+			code=self.rndchr()
+			draw.text((20*t+2, 0), code, font=font, fill=self.rndcolor_n())
+			code_list = code_list +code
+		# 模糊
+		# image = image.filter(ImageFilter.BLUR)
+		image.save('code.gif', 'gif')
+		# image.show ('code.gif')
+		return code_list
+		
+		# print(code_list)
 
-# 背景颜色
-def rndcolor_b():
-	return random.randint(127, 255), random.randint(127, 255), random.randint(127, 255)
-
-# 字体颜色
-def rndcolor_n():
-	return random.randint(50, 127), random.randint(50, 127), random.randint(50, 127)
-
-
-# 240*60
-width = 60 * 4
-height = 60
-image = Image.new('RGB', (width, height), (255, 255, 255))
-# 创建Font
-font = ImageFont.truetype('arial.ttf', 36)
-# 创建Draw
-draw = ImageDraw.Draw(image)
-# 填充像素
-for x in range(width):
-	for y in range(height):
-		draw.point((x, y), fill=rndcolor_b())
-# 输出文字
-for t in range(4):
-	draw.text((60*t+10, 10), rndchr(), font=font, fill=rndcolor_n())
-# 模糊
-image = image.filter(ImageFilter.BLUR)
-image.save('code.jpg', 'jpeg')
-image.show('code.jpg')
+if __name__ == '__main__':
+	code = CheckCode()
+	print(code.get_code())
